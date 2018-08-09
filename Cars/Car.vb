79e6e63d4c712f7c7d5cc7fe95e15b64b9b96b18
Rodolphe Saugier <rodolphe.saugier@gmail.com>
2018-08-09 13:37:41 +0200

    monolithic car

--- Cars/Car.vb
+++ Cars/Car.vb
@@ -1,32 +1,60 @@
 ﻿Public Class Car
     
-    Private EngineType As EngineType
-    
+#region "Fields"
+    Private _engineType As EngineType
+    Private _hasAbs As Boolean
+    Private _hasRegulator As Boolean
+#end Region
+
+#region "Public methods"
     Public Sub StartEngine()
         If EngineType.Thermical
             Warmup()
+        Else If EngineType.Electrical
+            CheckBatteryLevel()
         End If
     End Sub
     
     Public Sub StopEngine()
-        
+        If EngineType.Thermical
+            StartCoolingFan()
+        End If
+    End Sub
+    
+    Public Sub Accelerate(amount As Double)
+        If _hasRegulator
+            DisableRegulator()
+        End If
+    End Sub
+    
+    Public Sub Brake()
+        If _hasAbs Then
+            ActivateAbs()
+        End If
+    End Sub
+ #end Region
+
+ #region "Private methods"
+    Private Sub CheckBatteryLevel()
+        ' whatever
+    End Sub
+
+    Private Sub StartCoolingFan()
+        Throw New NotImplementedException
     End Sub
 
     Private Sub Warmup()
         'do stuff
     End Sub
-    
-    Public Sub Accelerate(amount As Double)
-        
+
+    Private Sub DisableRegulator()
     End Sub
-    
-    Public Sub SteerWheelToPosition(wheelPosition As Double)
-        
+
+    Private Sub ActivateAbs()
+        ' do stuff
     End Sub
-    
-    Public Sub Brake()
-        
-    End Sub
+#end Region  
+  
 End Class
 
 Friend Enum EngineType
