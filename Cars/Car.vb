eb438509619d3ebf26d26f7980a19b0375e41efb
Rodolphe Saugier <rodolphe.saugier@gmail.com>
2018-08-09 09:32:44 +0200

    add ocp

---
+++ Cars/Car.vb
@@ -0,0 +1,35 @@
+﻿Public Class Car
+    
+    Private EngineType As EngineType
+    
+    Public Sub StartEngine()
+        If EngineType.Thermical
+            Warmup()
+        End If
+    End Sub
+    
+    Public Sub StopEngine()
+        
+    End Sub
+
+    Private Sub Warmup()
+        'do stuff
+    End Sub
+    
+    Public Sub Accelerate(amount As Double)
+        
+    End Sub
+    
+    Public Sub SteerWheelToPosition(wheelPosition As Double)
+        
+    End Sub
+    
+    Public Sub Brake()
+        
+    End Sub
+End Class
+
+Friend Enum EngineType
+    Thermical
+    Electrical
+End Enum
