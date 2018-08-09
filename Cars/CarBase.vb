1ab3485a190e92d54fca6b9ae9bc65f5c4678024
Rodolphe Saugier <rodolphe.saugier@gmail.com>
2018-08-09 13:55:26 +0200

    monolithic car

---
+++ Cars/CarBase.vb
@@ -0,0 +1,47 @@
+﻿Public Class CarBase
+
+#region "Public methods"
+    Public Sub StartEngine()
+        DisplayEngineIndicator()
+        OnStartupEngine()
+    End Sub
+
+    Public Sub StopEngine()
+        OnStopEngine()
+        HideEngineIndicator()
+    End Sub
+
+    Public Sub Accelerate(amount As Double)
+        OnAccelerate()
+    End Sub
+    
+    Public Sub Brake()
+        OnBrake()
+    End Sub
+
+    Protected Overridable Sub OnBrake()
+    End Sub
+    
+    Protected Overridable Sub OnAccelerate()
+    End Sub
+    
+    Protected Overridable Sub OnStartupEngine()
+    End Sub
+    
+    Protected Overridable Sub OnStopEngine()
+    End Sub
+
+#end Region
+
+#region "Private methods"
+    Private Sub DisplayEngineIndicator()
+        Throw New NotImplementedException
+    End Sub
+    
+    Private Sub HideEngineIndicator()
+        Throw New NotImplementedException
+    End Sub
+#end Region  
+  
+End Class
+
---
+++ Cars/ElectricalCar.vb
@@ -0,0 +1,16 @@
+﻿Public Class ElectricalCar
+    Inherits CarBase
+
+    Protected Overrides Sub OnStartupEngine()
+        CheckBatteryLevel()
+    End Sub
+
+    Protected Overrides Sub OnStopEngine()
+        MyBase.OnStopEngine()
+    End Sub
+    
+    Private Sub CheckBatteryLevel()
+        ' do stuff
+    End Sub
+    
+End Class
